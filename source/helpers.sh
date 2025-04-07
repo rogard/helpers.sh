797519d8f75f316261e05c594cd1bc201f0f0cbe
rogard <erwann.rogard@gmail.com>
2025-04-07 16:54:41 -0400

    maintance throughout with examples, and some work on path

--- source/helpers.sh
+++ source/helpers.sh
@@ -10,92 +10,44 @@ c_erw_rule='#===================================================================
 
 erw_this="${BASH_SOURCE[0]}"
 function erw_help
-{
-    local pattern="${c_erw_rule}"
-    local string=$(sed -n "/^${pattern}$/,/^${pattern}$/p" "$erw_this")
+(
+    pattern="${c_erw_rule}"
+    string=$(sed -n "/^${pattern}$/,/^${pattern}$/p" "$erw_this")
     sed ":a; N; s/\(${pattern}\n\)\(${pattern}\)/\1/; ta; s/\n\{2,\}/\n/; P; D" <<< "${string}"
-}
+)
 #===============================================================================
-# erw_hash <trunc_digit> <string>
-#===============================================================================
-function erw_hash
-{
-    local trunc_digit="$1"
-    [[ "$trunc_digit" =~ ^[[:digit:]]+$ ]]\
-        || { echo "error: ${trunc_digit} not a digit"; return 1; }
-    local string="$2"
-    [[ -z "$string" ]]\
-        && { echo "error: empty string"; return 1; } 
-    echo "$string" |  md5sum | cut -d '-' -f1 | cut -c 1-"$trunc_digit"
-}
-#===============================================================================
-# c_erw_dns_regex_kv
 # c_erw_eml_regex_ar
 # c_erw_eml_regex
 #===============================================================================
-declare -A c_erw_dns_regex_kv
-c_erw_dns_regex_kv['local']='[[:alnum:]_.%+-]+'
-c_erw_dns_regex_kv['at']='@'
-c_erw_dns_regex_kv['sld']='[[:alnum:].-]+'
-c_erw_dns_regex_kv['tld']='\.[[:alpha:]]{2,}'
-declare -a c_erw_eml_regex_ar=('local' 'at' 'sld' 'tld')
-#  declare -r c_erw_eml_regex
-c_erw_eml_regex=$(for key in "${c_erw_eml_regex_ar[@]}";
-                  do printf '%s' "${c_erw_dns_regex_kv[$key]}"; 
-                  done)
+c_erw_dns_regex_ar=(
+    # local             at  sld              tld
+    '[[:alnum:]_.%+-]+' '@' '[[:alnum:].-]+' '\.[[:alpha:]]{2,}'
+)
+c_erw_eml_regex=$(printf '%s' "${c_erw_dns_regex_ar[@]}")
+#===============================================================================
+# c_erw_path_regex
+# Though not using them, FYI:
+# https://stackoverflow.com/a/10047501
+# https://stackoverflow.com/a/42036026
+#===============================================================================
+c_erw_path_regex='^[^[:cntrl:]]+$'
+#===============================================================================
+# erw_path_p <string>
+#===============================================================================
+function erw_path_p
+{
+    local string
+    string="${1}"
+    [[ "$string" =~ ^${c_erw_path_regex}$ ]]
+}
 #===============================================================================
 # erw_eml_address_p <string>
 #===============================================================================
 function erw_eml_address_p
-{
-    local address="$1"
-    local regex="^${c_erw_eml_regex}$" 
-    [[ $address =~ $regex ]] 
-}
-#===============================================================================
-# erw_fields_count <sep> <file>
-#===============================================================================
-function  erw_fields_count
-{
-    local sep="$1"
-    local file="$2"
-    awk -F"$sep" '{print NF}' "$file"
-}
-#===============================================================================
-# erw_path_join <parent> <child>
-#===============================================================================
-function erw_path_join
-{
-    local parent="${1%/}"
-    local child="$2"
-    local format="error: %s is empty"
-    [[ -z "$parent" ]] || { printf "$format" "$parent" ; return 1; }
-    [[ -z "$child" ]] || { printf "$format" "$child" ; return 1; }
-    echo "$parent/$child"
-}
-#===============================================================================
-# erw_path_ext <prefix> <ext>
-#===============================================================================
-function erw_path_ext
-{
-    local prefix="$1"
-    local ext="$2"
-    echo "${prefix}.${ext}"
-}
-#===============================================================================
-# erw_false
-#===============================================================================
-function erw_false
-{
-    false; echo "$?"
-}
-#===============================================================================
-# erw_true
-#===============================================================================
-function erw_true
-{
-    true; echo "$?"
-}
+(
+    string="${1}"
+    [[ "${string}" =~ ^${c_erw_eml_regex}$ ]] 
+)
 #===============================================================================
 # erw_false_p
 #===============================================================================
@@ -109,4 +61,75 @@ function erw_false_p
 function erw_true_p
 {
     (( $? == $(erw_true) ))
+}
+#===============================================================================
+# erw_fields_count <sep> <file>
+#===============================================================================
+function erw_fields_count
+{
+    local sep file
+    sep="$1"
+    file="$2"
+    awk -F"$sep" '{print NF}' "$file"
+}
+#===============================================================================
+# erw_false_p
+#===============================================================================
+function erw_false_p
+{
+    (( $? == $(erw_false) ))
+}
+#===============================================================================
+# erw_true_p
+#===============================================================================
+function erw_true_p
+{
+    (( $? == $(erw_false) ))
+}
+#===============================================================================
+# erw_path_join <parent> <child>
+#===============================================================================
+function erw_path_join
+{
+    local parent child result
+    parent="${1%/}"
+    child="${2#/}"
+    result="${parent}/${child}"
+    erw_path_p "${result}" \
+	|| {
+	format='error: %s does not match %s';
+	printf "$format" "$result" "";
+	return 1;
+    }
+    echo "${result}"
+}
+#===============================================================================
+# erw_hash <trunc_digit> <string>
+#===============================================================================
+function erw_hash
+{
+    local string
+    string="$1"
+    [[ -z "$string" ]]\
+        && { echo "error: empty string"; return 1; } 
+    echo "$string" |  md5sum | cut -d '-' -f1 # | cut -c 1-"$digit"
+}
+#===============================================================================
+# erw_tex_safe <string>
+#===============================================================================
+function erw_tex_safe
+{
+    local string
+    string="${1}"
+    echo "${string}" | sed 's/_/\\_/g'
+}
+#===============================================================================
+# erw_path_ext <prefix> <ext>
+#===============================================================================
+function erw_path_ext
+{
+    local prefix ext
+    prefix="$1"
+    ext="$2"
+    echo "${prefix}.${ext}"
 }
